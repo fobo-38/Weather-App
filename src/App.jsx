9891b1cd5b23b116858eaf931562bb1ad085f392
fobo <phoebe.alvarado38@gmail.com>
2025-07-08 00:46:51 -0400

    Finished Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+const API = {
+  key: "e791531f2b124ccc5a67a350f48b9cc7",
+  base: "https://api.openweathermap.org/data/3.0/"
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let month = months[d.getMonth()];
+    let date = d.getDate();
+    let year = d.getFullYear();
+
+    return `${day}, ${month} ${date}, ${year}`;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="background">
+        <main>
+          <div className="search_box">
+            <input type="text" className="search_text" placeholder="Search..."></input>
+          </div>
+          <div className="location_date">
+            <label className="location">New York City, US</label>
+            <label className="date">{dateBuilder(new Date())}</label>
+          </div>
+          <div className="center_display">
+            <label className="temp">90&deg;F</label>
+            <img src='src/icons/sun.png' className="weather_icon" alt="sun"></img>
+          </div>
+          <div className="weather">
+            <label className="weather_type">Sunny</label>
+          </div>
+          <div className="change_tab">
+            <button className="change_view">8-Day</button>
+          </div>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
